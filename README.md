75552fdf047bab2e2612f376b13c35be9e64735b
Wytalo Matheus <wytalo927@gmail.com>
2025-06-03 20:08:23 -0300

    Criação do Projeto

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # wms
 
+Esta Calculadora foi criada apenas como um Teste pratico dos meus conhecimentos
---
+++ calculadora.js
@@ -0,0 +1,12 @@
+//funão para adicionar valor ao visor
+function adicionar(valor){
+    document.getElementById('visor').value += valor;
+}
+
+function limpar(valor){
+    document.getElementById('visor').value = '';
+}
+
+function apagar(valor){
+    document.getElementById('visor');
+}
